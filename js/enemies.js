2dcdc8c429f407143fafc1ed0d0278bb027df0f9
AaronUnify2 <unify.goalsetter@gmail.com>
2026-01-26 09:48:13 +1000

    Update enemies.js

--- js/enemies.js
+++ js/enemies.js
@@ -230,7 +230,7 @@ function createWizardTexture() {
 
 // Enemy type definitions with varied stats
 const enemyTypes = [
-    { name: 'slime', texture: createSlimeTexture, scale: [2.5, 2.5], speed: 0.025, healthMult: 0.5, damageMult: 0.5, xp: 10, gold: 10, goldChance: 0.15 },
+    { name: 'slime', texture: createSlimeTexture, scale: [2.5, 2.5], speed: 0.025, healthMult: 0.5, damageMult: 0.5, xp: 10000, gold: 10000, goldChance: 0.99 },
     { name: 'goblin', texture: createGoblinTexture, scale: [2.5, 3.2], speed: 0.055, healthMult: 0.9, damageMult: 0.95, xp: 25, gold: 5, goldChance: 0.4 },
     { name: 'skeleton', texture: createSkeletonTexture, scale: [2.5, 4], speed: 0.038, healthMult: 1.8, damageMult: 1.05, xp: 30, gold: 8, goldChance: 0.5 },
     { name: 'ghost', texture: createGhostTexture, scale: [2.5, 3.2], speed: 0.1, healthMult: 3.4, damageMult: 1.2, xp: 35, gold: 10, goldChance: 0.3 },
